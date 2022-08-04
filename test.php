43bbdc90a3c46134e39145b9379239b762d63368
Noha Salah <101585161+NohaEltohamy@users.noreply.github.com>
2022-08-04 17:20:01 +0200

    Update test.php from Remote Repo

--- test.php
+++ test.php
@@ -2,4 +2,21 @@
 
 echo "Welcome to GIT coure";
 
+$x=true;
+$z ="I LOVE PHP";
+
+print_r($_SERVER['PORT']);
+$x=true;
+$z ="I LOVE PHP";
+
+print_r($_SERVER['PORT']);
+$x=true;
+$z ="I LOVE PHP";
+
+print_r($_SERVER['PORT']);
+$x=true;
+$z ="I LOVE PHP";
+
+print_r($_SERVER['PORT']);
+
 ?>
